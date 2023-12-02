69f0d4b9ea003c1d4e64dc1b9744bccd63605e77
DivyaUtlapalli <149135754+DivyaUtlapalli@users.noreply.github.com>
2023-12-01 23:11:16 -0500

    Add models and Routes

---
+++ server/models/users.js
@@ -0,0 +1,17 @@
+const users =[
+    {
+        "username":"Divya",
+        "password": "Abc@1234"
+    },
+    {
+        "username":"Divya22",
+        "password": "Abc@12340" 
+    },
+    {
+        "username":"DivyaU",
+        "password": "Abc@1234567" 
+    },
+]
+exports.getAllUsers = ()=>{
+    return users;
+}
